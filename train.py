2da98a4ddd3a89c606e7aaf1bb12ea93e7ff8217
kcirscross <68851460+kcirscross@users.noreply.github.com>
2022-11-06 22:06:26 +0700

    test

--- train.py
+++ train.py
@@ -62,6 +62,7 @@ hidden_size = 8
 output_size = len(tags)
 print(input_size, output_size)
 
+
 class ChatDataset(Dataset):
 
     def __init__(self):
@@ -76,6 +77,7 @@ class ChatDataset(Dataset):
     # we can call len(dataset) to return the size
     def __len__(self):
         return self.n_samples
+
 
 dataset = ChatDataset()
 train_loader = DataLoader(dataset=dataset,
@@ -96,18 +98,18 @@ for epoch in range(num_epochs):
     for (words, labels) in train_loader:
         words = words.to(device)
         labels = labels.to(dtype=torch.long).to(device)
-        
+
         # Forward pass
         outputs = model(words)
         # if y would be one-hot, we must apply
         # labels = torch.max(labels, 1)[1]
         loss = criterion(outputs, labels)
-        
+
         # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
+
     if (epoch+1) % 100 == 0:
         print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
 
